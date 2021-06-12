6e26049e8861efe8946ee2ff9bb4cea0818193ce
Pieter Swinkels <swinkels.pieter@yahoo.com>
2021-06-12 23:26:02 +0200

    Update package file

--- nutter-pkg.el
+++ nutter-pkg.el
@@ -1,2 +1,4 @@
-(define-package "nutter" "0.0.0" "Provide commands to search directories of org files"
-  '((helm-org-rifle "0")))
+(define-package "nutter" "0.0.0" "Create and search notes in directories of org files"
+  '((yasnippet "0")
+    (helm-org-rifle "0")
+    (f "0")))
